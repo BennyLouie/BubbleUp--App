ebdf75a8c814fc3a23b9783267a471175419347e
BennyLouie <bennylouie1412@gmail.com>
2019-10-03 18:34:40 -0400

    a bunch of css that i do not fully understand

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: 'logout'
-  resource :sessions, only: [:new, :create, :destroy]
   resources :recommendations
   resources :shop_reviews
   resources :shops, only: [:index, :show, :new, :create]
